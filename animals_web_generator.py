3d3096e9001c6950cedc9f4c414cb34a6d5727af
Dein Name <deine@email.de>
2026-03-08 20:44:07 +0100

    Tierdaten in HTML einfügen

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,10 +2,34 @@ import json
 
 def load_data(file_path):
     """ Loads a JSON file """
-    with open(file_path, "r") as handle:
+    with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
 animals_data = load_data('animals_data.json')
+
+with open("animals_template.html", "r", encoding="utf-8") as file:
+    html_template = file.read()
+
+output = ""
+
+for animal in animals_data:
+    output += f"Name: {animal['name']}\n"
+
+    if "characteristics" in animal and "diet" in animal["characteristics"]:
+        output += f"Diet: {animal['characteristics']['diet']}\n"
+
+    if "locations" in animal:
+        output += f"Location: {animal['locations'][0]}\n"
+
+    if "characteristics" in animal and "type" in animal["characteristics"]:
+        output += f"Type: {animal['characteristics']['type']}\n"
+
+    output += "\n"
+
+html_output = html_template.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open("animals.html", "w", encoding="utf-8") as file:
+    file.write(html_output)
 
 for animal in animals_data:              # Iteriert durch alle Tiere in der Liste
 
