c740285ac165b5ef66b079987d1cb515cc0807b4
Dein Name <deine@email.de>
2026-03-09 00:39:08 +0100

    code formatieren und serialize_animal Funktion erstellen

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,9 +1,32 @@
 import json
 
+
 def load_data(file_path):
-    """ Loads a JSON file """
+    """Loads a JSON file"""
     with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
+
+
+def serialize_animal(animal):
+    output = ''
+    output += '<li class="cards__item">\n'
+    output += f'<div class="card__title">{animal["name"]}</div>\n'
+    output += '<p class="card__text">\n'
+
+    if "characteristics" in animal and "diet" in animal["characteristics"]:
+        output += f'<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
+
+    if "locations" in animal:
+        output += f'<strong>Location:</strong> {animal["locations"][0]}<br/>\n'
+
+    if "characteristics" in animal and "type" in animal["characteristics"]:
+        output += f'<strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
+
+    output += '</p>\n'
+    output += '</li>\n'
+
+    return output
+
 
 animals_data = load_data('animals_data.json')
 
@@ -13,39 +36,9 @@ with open("animals_template.html", "r", encoding="utf-8") as file:
 output = ""
 
 for animal in animals_data:
-    output += f"Name: {animal['name']}<br/>\n"
-
-    if "characteristics" in animal and "diet" in animal["characteristics"]:
-        output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
-
-    if "locations" in animal:
-        output += f"Location: {animal['locations'][0]}<br/>\n"
-
-    if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f"Type: {animal['characteristics']['type']}<br/>\n"
-
-    output += '</li>\n'
+    output += serialize_animal(animal)
 
 html_output = html_template.replace("__REPLACE_ANIMALS_INFO__", output)
 
 with open("animals.html", "w", encoding="utf-8") as file:
     file.write(html_output)
-
-output = ""
-
-for animal in animals_data:
-    output += '<li class="cards__item">\n'
-    output += f'<div class="card__title">{animal["name"]}</div>\n'
-    output += '<p class="card__text">\n'
-
-    if "characteristics" in animal and "diet" in animal["characteristics"]:
-        output += f"<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
-
-    if "locations" in animal:
-        output += f"<strong>Location:</strong> {animal['locations'][0]}<br/>\n"
-
-    if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f"<strong>Type:</strong> {animal['characteristics']['type']}<br/>\n"
-
-    output += '</p>\n'
-    output += '</li>\n'
